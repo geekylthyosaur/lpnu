1343fc4d0336f6518a5f7876f00dbed96d58ed4e
geekylthyosaur <geekylthyosaur@gmail.com>
2022-09-23 22:52:54 +0300

    OS lab4: upd

--- OS/lab4/main.c
+++ OS/lab4/main.c
@@ -4,33 +4,36 @@
 #include <errno.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+
 int main(void) {
     pid_t pid;
-    int status;
-    pid = fork();
-    switch(pid) {
-        case -1:
-            // якщо fork() повернув -1, то процес не створився
-            perror("fork");
-            break;
-        case 0:
-            // якщо fork() повернув 0, то тут пишемо інструкції для
-            //дочірнього процесу
-            // тут можна виконати заміну на іншу програму через exec
-            printf("my pid=%i, returned pid=%i, my parent’s pid= %i\n", getpid(), pid, getppid());
-            break;
-        default:
-            // якщо fork() повернув число> 0, то тут пишемо інструкції для
-            //батьківського процесу
-            printf("my pid = %i, returned pid = %i\n", getpid(), pid);
-            // якщо дочірній завершився а батьківський не чекає коду його
-            //завершення, то це створює процес-зомбі
-            if (waitpid(pid, &status,0)!=0)
-                printf("childprocess ended with status %i\n", status);
-            else
-                perror("waitpid");
-            kill(pid, SIGKILL);
-            break;
+    int status, PC = 0;
+
+    printf("Enter number of processes: ");
+    while (PC < 1)
+        scanf("%d", &PC);
+
+    int _ = 0;
+
+    for (int i = 0; i < PC; i++) {
+        if (pid > 0)
+            pid = fork();
+        switch(pid) {
+            case -1:
+                // Error
+                break;
+            case 0:
+                printf("%d: Start\n", getpid());
+                return 0;
+            default:
+                if (waitpid(pid, &status,0)!=0)
+                    printf("%d: End\n", pid);
+                else
+                //Error
+                break;
+        }
     }
+
     return 0;
 }
+
--- OS/lab4/process.c
+++ OS/lab4/process.c
@@ -1,2 +1,37 @@
-int main() {
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main(void) {
+    pid_t pid;
+    int status, PC = 0;
+
+    printf("Enter number of processes: ");
+    while (PC < 1)
+        scanf("%d", &PC);
+
+    for (int i = 0; i < PC; i++) {
+        if (pid > 0)
+            pid = fork();
+        switch(pid) {
+            case -1:
+                perror("fork");
+                break;
+            case 0:
+                printf("%d: Start\n", getpid());
+                return 0;
+            default:
+                if (waitpid(pid, &status,0)!=0)
+                    printf("%d: End\n", pid);
+                else
+                    perror("waitpid");
+                break;
+        }
+    }
+
+    return 0;
 }
+

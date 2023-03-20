a70621ae6d3761ecc90d25a889b103d3252d0fe0
geekylthyosaur <geekylthyosaur@gmail.com>
2023-03-20 21:23:33 +0200

    Upd.

---
+++ WEB/form/script.js
@@ -0,0 +1,60 @@
+const form = document.getElementById("rental-form");
+const licenseCheckbox = document.getElementById("license");
+const driverInfo = document.getElementById("driver-info");
+
+licenseCheckbox.addEventListener("change", function() {
+  if (this.checked) {
+    driverInfo.style.display = "none";
+  } else {
+    driverInfo.style.display = "block";
+  }
+});
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  if (validateForm()) {
+    alert("Your car has been rented successfully!");
+    this.reset();
+  } else {
+    alert("Please check correctness of marked fields.");
+  }
+});
+
+function validateForm() {
+  let valid = true;
+
+  // Check required fields
+  const requiredFields = ["name", "surname"];
+
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = document.getElementById(requiredFields[i]);
+    if (!/^[a-zA-Z ]+$/.test(field.value.trim())) {
+      valid = false;
+      field.classList.add("invalid");
+    } else {
+      field.classList.remove("invalid");
+    }
+  }
+
+  const today = new Date();
+  const start = document.getElementById("start-date");
+  const end = document.getElementById("end-date");
+  const startDate = new Date(document.getElementById("start-date").value);
+  const endDate = new Date(document.getElementById("end-date").value);
+
+  if (startDate > endDate || startDate < today || endDate < today) {
+    valid = false;
+    start.classList.add("invalid");
+    end.classList.add("invalid");
+  } else {
+    start.classList.remove("invalid");
+    end.classList.remove("invalid");
+  }
+
+  return valid;
+}
+
+function cancel() {
+  form.reset();
+}

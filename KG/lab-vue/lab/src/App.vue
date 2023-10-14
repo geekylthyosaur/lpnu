18a2abfb43bfa2c64014d802bdce7d92e731fddd
geekylthyosaur <geekylthyosaur@gmail.com>
2023-10-14 12:59:01 +0300

    Upd.

---
+++ KG/lab-vue/lab/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Main/>
+</template>
+
+<script>
+import Main from './components/Main.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style>
+html {
+  width: 800px;
+  height: 600px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 2px solid #000;
+}
+</style>
---
+++ KG/lab-vue/lab/src/components/Main.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="d-flex" style="height: 600px; width: 800px;">
+    <div class="col align-self-center" style="width: 343px;">
+      <div class="row d-flex justify-content-center">
+        <div class="btn btn-custom d-inline-flex align-items-center" :class="{ active: showPopup }" @click="showPopup = !showPopup">
+          <span>Фрактали</span>
+          <i class="fa fa-chevron-right right-arrow" v-if="!showPopup"></i>
+        </div>
+        <div class="btn btn-custom d-inline-flex align-items-center">
+          Колірні схеми
+          <i class="fa fa-chevron-right right-arrow"></i>
+        </div>
+        <div class="btn btn-custom d-inline-flex align-items-center">
+          Рухомі зображення
+          <i class="fa fa-chevron-right right-arrow"></i>
+        </div>
+        <hr style="width: 90%; height: 3px; border: none; background-color: #bebebe;">
+        <div class="btn btn-custom d-inline-flex align-items-center">
+          Матеріали
+          <i class="fa fa-chevron-right right-arrow"></i>
+        </div>
+        <div class="btn btn-custom d-inline-flex align-items-center">
+          Опитувальник
+          <i class="fa fa-chevron-right right-arrow"></i>
+        </div>
+      </div>
+    </div>
+    <div class="vr align-self-center" style="height: 100%; width: 3px; border: none; background-color: #bebebe;"></div>
+    <div class="col align-self-center">
+      <div class="row d-flex justify-content-center">
+        <div class="text-center" style="font-size: 32px;">
+          ABC
+        </div>
+      </div>
+    </div>
+    <div class="popup" v-if="showPopup">
+      <div class="btn btn-custom d-inline-flex align-items-center" style="background-color: white;">
+        Фрактал Мандельброта
+        <i class="fa fa-chevron-right right-arrow"></i>
+      </div>
+      <div class="btn btn-custom d-inline-flex align-items-center" style="background-color: white;margin-bottom:0px;">
+        Броунівський рух
+        <i class="fa fa-chevron-right right-arrow"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainWindow',
+  data() {
+    return {
+      showPopup: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.btn-custom {
+  font-size: 22px;
+  margin-bottom: 13px;
+  border-radius: 24px;
+  height: 56px;
+  width: 306px;
+  background-color: #ebebeb;
+  border: 3px solid #ebebeb;
+}
+.btn:active {
+  border: 3px solid #bebebe;
+  background-color: #ebebeb;
+}
+.active {
+  border: 3px solid #bebebe;
+  background-color: #ebebeb;
+}
+.right-arrow {
+  color: #bebebe;
+  font-size: 32px;
+  margin-left: auto;
+}
+.popup {
+  position: absolute;
+  top: 70px;
+  right: 100px;
+  width: 332px;
+  background-color: #ebebeb;
+  padding: 13px;
+  border-radius: 24px;
+}
+.popup::before {
+  content: '';
+  position: absolute;
+  top: 50%;
+  right: 100%;
+  transform: translateY(-50%);
+  border: solid transparent;
+  border-width: 8px;
+  border-right-color: #ebebeb;
+}
+</style>

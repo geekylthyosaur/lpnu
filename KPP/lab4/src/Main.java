f97801c94b356ad2acd2f2af634ca0d6ac0f9d80
geekylthyosaur <geekylthyosaur@gmail.com>
2023-10-24 22:33:45 +0300

    Upd.

---
+++ KPP/lab4/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Get start time
+        // Get end time
+        // Get working days as list of holidays per month
+        // Get working hours per day
+        // Calc total number of working hours
+        System.out.println("Hello world!");
+    }
+}

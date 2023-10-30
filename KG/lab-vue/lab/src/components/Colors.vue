3946fe7da43273b5dd4a7e66f723209af17a2e2b
geekylthyosaur <geekylthyosaur@gmail.com>
2023-10-30 23:19:10 +0200

    Upd.

--- KG/lab-vue/lab/src/components/Colors.vue
+++ KG/lab-vue/lab/src/components/Colors.vue
@@ -10,30 +10,29 @@
   </div>
   <div v-if="!isLoaded" class="d-flex justify-content-center align-items-center" style="height: 540px; width: 800px;">
     <div class="col" style="max-width: 343px;">
-  <div class="row d-flex justify-content-center">
-    <div class="btn btn-custom d-flex justify-content-center" style="margin-top: 13px;">
-      Вибрати колір
-      <input style="margin-left: 30px; margin-top: 3px;" type="color" value="#400000" name="" id="color" @change="generatePalette(); generateImage()">
+      <div class="row d-flex justify-content-center">
+        <div class="btn btn-custom d-flex justify-content-center" style="margin-top: 13px;">
+          Вибрати колір
+          <input style="margin-left: 30px; margin-top: 3px;" type="color" value="#400000" name="" id="color" @change="generatePalette(); generateImage()">
+        </div>
+      </div>
+      <div class="row d-flex justify-content-center" style="margin-top: 367px;">
+        <div class="btn btn-custom" @click="saveImage">Зберегти зображення</div>
+      </div>
     </div>
-  </div>
-  <div class="row d-flex justify-content-center" style="margin-top: 367px;">
-    <div class="btn btn-custom" @click="saveImage">Зберегти зображення</div>
-  </div>
-</div>
-<div class="vr align-self-center" style="height: 100%; width: 3px; border: none; background-color: #bebebe;"></div>
-<div class="col align-self-center">
-  <div class="row d-flex justify-content-center">
-    <div class="d-flex justify-content-center align-items-center" style="width: 400px; height: 400px; position: relative;">
-      <canvas id="leftCanvas" @mousedown="startResize" width="400" height="400" style="width: 50%; height: 100%; background-color: blue;"></canvas>
-      <canvas id="rightCanvas" @mousedown="startResize" width="400" height="400" style="width: 50%; height: 100%; background-color: red;"></canvas>
+    <div class="vr align-self-center" style="height: 100%; width: 3px; border: none; background-color: #bebebe;"></div>
+    <div class="col align-self-center">
+      <div class="row d-flex justify-content-center">
+        <div class="d-flex justify-content-center align-items-center" style="width: 400px; height: 400px; position: relative;">
+          <canvas ref="leftCanvas" @mousedown="startResize" width="400" height="400" style="width: 50%; height: 100%; background-color: blue;"></canvas>
+          <canvas ref="rightCanvas" @mousedown="startResize" width="400" height="400" style="width: 50%; height: 100%; background-color: red;"></canvas>
+        </div>
+        <div class="d-flex justify-content-center">
+          <div class="align-items-left" style="margin-right: 300px; color: #bebebe; font-size: small;">До</div>
+          <div class="align-items-right" style="color: #bebebe; font-size: small;">Після</div>
+        </div>
+      </div>
     </div>
-    <div class="d-flex justify-content-center">
-      <div class="align-items-left" style="margin-right: 300px; color: #bebebe; font-size: small;">До</div>
-      <div class="align-items-right" style="color: #bebebe; font-size: small;">Після</div>
-    </div>
-  </div>
-</div>
-
   </div>
 </template>
 
@@ -69,21 +68,19 @@ export default {
     resize(e) {
       if (this.resizing) {
         const diffX = e.clientX - this.initialX;
-        let leftCanvas = document.getElementById("leftCanvas");
-        let rightCanvas = document.getElementById("rightCanvas");
         let newLeftWidth = this.leftCanvasWidth + (diffX / 400) * 100;
         let newRightWidth = 100 - newLeftWidth;
         if (newLeftWidth > 90) newLeftWidth = 90;
         if (newRightWidth > 90) newRightWidth = 90;
         if (newLeftWidth < 10) newLeftWidth = 10;
         if (newRightWidth < 10) newRightWidth = 10;
-        leftCanvas.style.width = `${newLeftWidth}%`;
-        rightCanvas.style.width = `${newRightWidth}%`;
+        this.$refs.leftCanvas.style.width = `${newLeftWidth}%`;
+        this.$refs.rightCanvas.style.width = `${newRightWidth}%`;
       }
     },
     stopResize() {
       this.resizing = false;
-      this.leftCanvasWidth = parseFloat(document.getElementById("leftCanvas").style.width);
+      this.leftCanvasWidth = parseFloat(this.$refs.leftCanvas.style.width);
     },
   }
 }
